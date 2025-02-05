997b41776f08da915137f6ccb74a874669bc69c3
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-04 23:17:45 -0300

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,58 @@ import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
+
 import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'hhtps://github.com/GKsegura.png',
+      name: 'José Segura',
+      role: 'Front-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-02-02 08:15:00'), // 2 de fevereiro às 08:15
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/AndreBetetto.png',
+      name: 'André Betetto',
+      role: 'Back-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-01-20 10:30:00'), // 20 de janeiro às 10:30
+  },
+];
 
 export function App() {
   return (
     <div>
       <Header />
-    <div className={styles.wrapper}>
-    <Sidebar />
-      <main>
-        <Post />
-        <Post />
-      </main>
-    </div>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,47 +1,49 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post() {
-return (
-    <article className={styles.post}>
-        <header>
-            <div className={styles.author}>
-                <Avatar src="https://github.com/GKsegura.png"/>
-                <div className={styles.authorInfo}>
-                    <strong>José Segura</strong>
-                    <span>Web Developer</span>
+export function Post(props) {
+    console.log(props);
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src="https://github.com/GKsegura.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>José Segura</strong>
+                        <span>Web Developer</span>
+                    </div>
                 </div>
-            </div>
 
-            <time title='2 de fevereiro às 08:15' dateTime='2025-02-02 08:15:00'>Publicado há 1h</time>
-        </header>
+                <time title='2 de fevereiro às 08:15' dateTime='2025-02-02 08:15:00'>Publicado há 1h</time>
+            </header>
 
-        <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
 
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
 
-            <p>
-                <a href="">#novoprojeto</a>{' '}
-                <a href="">#nlw</a>{' '}
-                <a href="">#rocketseat</a>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
                 </p>
-        </div>
-        <form className={styles.commentForm}>
-            <strong>Deixe seu feedback</strong>
-            <textarea placeholder='Deixe um comentário'/>
-            <footer>
-                <button type='submit'>Publicar</button>
-            </footer>
-        </form>
-        <div className={styles.commentList}>
+            </div>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe um comentário' />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
                 <Comment />
                 <Comment />
                 <Comment />
-        </div>
-    </article>
-)
+            </div>
+        </article>
+    )
 }
